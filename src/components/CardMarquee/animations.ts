4d9070c9ea95088f6ecb7645816ba740feb32c48
Christian Dunne <yo@modular-everything.com>
2024-02-19 16:53:10 +0000

    fix: Stagger in cards

--- src/components/CardMarquee/animations.ts
+++ src/components/CardMarquee/animations.ts
@@ -1,30 +1,25 @@
 import gsap from "gsap";
 
-export function animateMediumCards(card: string) {
-  gsap.defaults({
-    ease: "power2.inOut",
-    duration: 1.25,
-    transformOrigin: "center center",
-    // stagger: function (index) {
-    //   if (index === 0) {
-    //     return 0.5;
-    //   }
-    //   if (index === 1) {
-    //     return 1.25;
-    //   }
-    //   if (index === 2) {
-    //     return 0.25;
-    //   }
-    //   if (index === 3) {
-    //     return 1;
-    //   }
-    //   if (index > 3 && index < 8) {
-    //     return 0.5;
-    //   }
-    //   return 0;
-    // },
-  });
+function getStagger(index: number) {
+  if (index === 0) {
+    return 0.5;
+  }
+  if (index === 1) {
+    return 1.25;
+  }
+  if (index === 2) {
+    return 0.25;
+  }
+  if (index === 3) {
+    return 1;
+  }
+  if (index > 3 && index < 8) {
+    return 0.5;
+  }
+  return 0;
+}
 
+export function animateMediumCards(card: string) {
   return gsap.fromTo(
     // stagger in the cards, starting with the first card and scaling down to the last card
     card,
@@ -35,6 +30,10 @@ export function animateMediumCards(card: string) {
     {
       scale: 1,
       autoAlpha: 1,
+      ease: "power2.out",
+      duration: 1.25,
+      transformOrigin: "center center",
+      stagger: getStagger,
     }
   );
 }
