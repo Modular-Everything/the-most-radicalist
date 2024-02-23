e27a0a6da085caeeb13415aa4cc4a68b3b9182e8
Christian Dunne <yo@modular-everything.com>
2024-02-23 14:43:35 +0000

    feat: Add Header with new theme toggle

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,20 @@
 "use client";
 
+import clsx from "clsx";
+import Link from "next/link";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 
+import { GridIcon } from "@/icons/GridIcon";
+import { HomeIcon } from "@/icons/HomeIcon";
+import { Logo } from "@/icons/Logo";
 import { MoonIcon } from "@/icons/MoonIcon";
+import { PersonIcon } from "@/icons/PersonIcon";
 import { SunIcon } from "@/icons/SunIcon";
 
-function ThemeToggle() {
+import styles from "./Header.module.scss";
+
+function ThemeToggle({ className }: { className: string }) {
   const { resolvedTheme, setTheme } = useTheme();
   const otherTheme = resolvedTheme === "dark" ? "light" : "dark";
   const [mounted, setMounted] = useState<boolean>(false);
@@ -19,7 +27,7 @@ function ThemeToggle() {
     <button
       type="button"
       onClick={() => setTheme(otherTheme)}
-      // className={styles.header__lightToggle}
+      className={className}
       aria-label={mounted ? `Switch to ${otherTheme} theme` : "Toggle theme"}
     >
       {resolvedTheme === "dark" ? <SunIcon /> : <MoonIcon />}
@@ -29,8 +37,32 @@ function ThemeToggle() {
 
 export function Header() {
   return (
-    <header>
-      <ThemeToggle />
+    <header className={styles.header}>
+      <Link href="/" className={clsx("logo", styles.header__logo)}>
+        <Logo />
+      </Link>
+
+      <nav role="navigation">
+        <ul className={styles.header__nav}>
+          <li>
+            <Link href="/" className={styles.header__nav__icon}>
+              <HomeIcon />
+            </Link>
+          </li>
+          <li>
+            <Link href="/" className={styles.header__nav__icon}>
+              <PersonIcon />
+            </Link>
+          </li>
+          <li>
+            <Link href="/" className={styles.header__nav__icon}>
+              <GridIcon />
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      <ThemeToggle className={styles.header__lightToggle} />
     </header>
   );
 }
