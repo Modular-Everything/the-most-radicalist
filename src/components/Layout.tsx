e6496f4878d82100c29f8a44b869aff69116dbd9
Christian Dunne <yo@modular-everything.com>
2024-02-23 14:28:31 +0000

    chore: Remove unused elements

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,17 +6,10 @@ import { Header } from "@/components/Header";
 export function Layout({ children }: { children: ReactNode }) {
   return (
     <>
-      {/* <div className="fixed inset-0 flex justify-center sm:px-8">
-        <div className="flex w-full max-w-7xl lg:px-8">
-          <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
-        </div>
-      </div> */}
-      <div>
-        <Header />
-        {/* <main className="flex-auto">{children}</main> */}
-        <main>{children}</main>
-        {/* <Footer /> */}
-      </div>
+      <Header />
+      {/* <main className="flex-auto">{children}</main> */}
+      <main>{children}</main>
+      {/* <Footer /> */}
     </>
   );
 }
